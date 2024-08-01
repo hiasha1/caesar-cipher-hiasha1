c0b9b700bb6b47d7959319c4832cc3fa6bbc2510
hiasha1 <95436084+hiasha1@users.noreply.github.com>
2024-08-01 11:15:47 -0400

    new code added

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
-# add your code here
+
+text = input('please enter the string \n')
+shift = 5
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
+
+for char in text.lower():
+    if char == ' ':
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = (index + shift) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+print('encrypted_text:', encrypted_text)
+
+
