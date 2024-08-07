3ad509a2cdfaddadd7119d70efa58f07295edf7a
hiasha1 <95436084+hiasha1@users.noreply.github.com>
2024-08-06 23:01:53 -0400

    code modified

--- cipher.py
+++ cipher.py
@@ -1,16 +1,19 @@
 
+
 text = input('please enter the string \n')
 shift = 5
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 
 for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
+    
+    if char.isalpha()==False:
+        print(char, end = '' )
+        
     else:
         index = alphabet.find(char)
         new_index = (index + shift) % len(alphabet)
-        encrypted_text += alphabet[new_index]
-print('encrypted_text:', encrypted_text)
+        encrypted_text = alphabet[new_index]
+        print( encrypted_text ,end = '')
 
 
